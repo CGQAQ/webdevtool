aa1ce7559682efebfefef2e98a07539c3c6a6a63
Jason <m.jason.liu@outlook.com>
2021-01-25 14:56:26 +0800

    change create new tab's behavior

--- ui/src/reducers/tabs.ts
+++ ui/src/reducers/tabs.ts
@@ -1,59 +1,80 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const TABSLICE_NAME = "tabs";
 
 export type Tabs = Array<Tab>
 
-const TITLE_DEFAULT = 'New Tab';
-const DATA_DEFAULT: TabData = Object.freeze({
-    addr: '',
-    port: 0,
-    protocol: 'http',
-    isClient: true
-});
+const TITLE_DEFAULT = "New Tab";
+
 
 export interface Tab {
-    id: number,
-    title: string,
-    data: TabData
+  id: number,
+  title: string,
+  data: TabData
 }
 
 export interface TabData {
-    addr: string,
-    port: number,
-    protocol: string,
-    isClient: boolean,
+  addr: string,
+  port: number,
+  protocol: string,
+  isClient: boolean,
+}
+
+function defaultData() {
+  return {
+    addr: "",
+    port: 0,
+    protocol: "http",
+    isClient: true,
+  };
 }
 
 function generateTab(): Tab {
-    return {
-        id: Date.now(),
-        title: TITLE_DEFAULT,
-        data: DATA_DEFAULT,
-    }
+  return {
+    id: Date.now(),
+    title: TITLE_DEFAULT,
+    data: defaultData(),
+  };
+}
+
+interface InitialStateType {
+  tabs: Tabs,
+  current: number,
+}
+
+function initialState(): InitialStateType {
+  const now = Date.now();
+  return {
+    tabs: [{
+      id: now,
+      title: TITLE_DEFAULT,
+      data: defaultData(),
+    }] as Tabs, current: now,
+  };
 }
 
 const tabSlice = createSlice({
-    name: TABSLICE_NAME,
-    extraReducers: undefined,
-    initialState: [{
-        id: Date.now(),
-        title: TITLE_DEFAULT,
-        data: DATA_DEFAULT,
-    }] as Tabs,
-    reducers: {
-        new(state) {
-            state.push(generateTab());
-        },
-        remove(state, action: PayloadAction<number>) {
-          return state.filter(it => it.id !== action.payload);
-        },
-        rename(state, action: PayloadAction<{id: number, title: string}>) {
-          const tab = state.find(it => it.id === action.payload.id);
-          if(tab && tab.title)
-            tab.title = action.payload.title;
-        }
+  name: TABSLICE_NAME,
+  extraReducers: undefined,
+  initialState: initialState(),
+  reducers: {
+    new(state) {
+      const newTab = generateTab();
+      state.tabs.push(newTab);
+      state.current = newTab.id;
     },
+    remove(state, action: PayloadAction<number>) {
+      return { tabs: state.tabs.filter(it => it.id !== action.payload), current: state.current };
+    },
+    rename(state, action: PayloadAction<{ id: number, title: string }>) {
+      const tab = state.tabs.find(it => it.id === action.payload.id);
+      if (tab && tab.title)
+        tab.title = action.payload.title;
+    },
+    changeCurrent(state, action: PayloadAction<number>) {
+      state.current = action.payload;
+    },
+  },
 });
 
 export default tabSlice;
